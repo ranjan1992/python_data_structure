8068f6739c8b053412cdb5d5ee3208ad6dfd0bc3
Ranjan Verma <verma.ranjan62@gmail.com>
2022-06-08 15:49:53 +0530

    Updated Linked List 1 problems

--- LINKED LIST_1/10. APPEND_LAST_N_TO_FIRST.py
+++ LINKED LIST_1/10. APPEND_LAST_N_TO_FIRST.py
@@ -1,56 +1,68 @@
-from sys import stdin
 
-class Node:
-    def __init__(self, data):
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
         self.data = data
         self.next = None
+
+
+
+
+def printReverse(head,arr):
+    if head is None:
+        for i in arr[::-1]:
+            print(i,end=" ")
+        return
+    arr.append(head.data)
+    # print(arr, head.data)
+    printReverse(head.next,arr)
     
-def appendLastNToFirst(head, n):
-    if n==0 or head is None:
-        return head
     
-    fast = head
-    slow = head
-    initialHead = head
-
-    for i in range(n):
-        fast = fast.next
-
-    while fast.next is not None :
-        slow=slow.next
-        fast= fast.next
-    
-    temp = slow.next
-    slow.next = None
-    fast.next = initialHead
-    head = temp
-
-    return head
-
-def takeInput():
+#Taking Input Using Fast I/O
+def takeInput() :
     head = None
-    tail= None
+    tail = None
 
-    datas= list(map(int , stdin.readline().rstrip().split(" ")))
-    i=0
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
 
-    while(i< len(datas)) and (datas[i]!=-1):
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
         data = datas[i]
         newNode = Node(data)
 
-        if head is None:
+        if head is None :
             head = newNode
-            tail= newNode
-        
-        else:
+            tail = newNode
+
+        else :
             tail.next = newNode
-            tail= newNode
-        i+=1
+            tail = newNode
+
+        i += 1
+
     return head
 
-def printLinkedList(head):
+
+#to print the linked list 
+def printLinkedList(head) :
 
     while head is not None :
-        print(head.data, end=" ")
+        print(head.data, end = " ")
         head = head.next
 
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    arr = []
+    head = takeInput()
+    printReverse(head,arr)
+    print()
+
+    t -= 1
