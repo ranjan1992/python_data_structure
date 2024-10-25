b957ff745a03c71e1de369c85eff4b6f2e287505
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2024-10-25 13:34:06 +0530

    Changed some more orders

--- 01_RECURSION/1_EXAMPLE.py
+++
@@ -1,9 +0,0 @@
-def fact(n):
-    if n==0:
-        return 1
-    smaddllOp=fact(n-1)
-    return n*smaddllOp
-
-n=int(input())
-print(fact(n))
-
--- 01_RECURSION/2_SUM_N.py
+++
@@ -1,9 +0,0 @@
-def sum_n(n):
-    if n==0:
-        return 0
-    smallOp= sum_n(n-1)
-    output=smallOp+n
-    return output
-
-n=int(input())
-print(sum_n(n))
